7a32c9c636222f5af39d6d98cc9c2f81dad8eac2
BloodEko <1sicherheit@gmx.de>
2021-04-25 15:54:42 +0200

    use local MapView as default

--- src/main/java/de/bloodeko/towns/cmds/map/MapCmd.java
+++ src/main/java/de/bloodeko/towns/cmds/map/MapCmd.java
@@ -1,22 +1,18 @@
 package de.bloodeko.towns.cmds.map;
 
-import java.util.Arrays;
-import java.util.List;
-
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import de.bloodeko.towns.cmds.CmdBase;
 import de.bloodeko.towns.town.ChunkMap;
 import de.bloodeko.towns.town.Town;
-import de.bloodeko.towns.util.Messages;
-import de.bloodeko.towns.util.Util;
+import de.bloodeko.towns.util.Chunk;
 
 /**
  * Handles requests to provide a view implementation and
  * adds the player to the click listener service.
  */
 public class MapCmd extends CmdBase {
-    private final String localname = Messages.get("cmds.map.local");
     private MapListener listener;
     
     public MapCmd(MapListener listener) {
@@ -25,12 +21,16 @@ public class MapCmd extends CmdBase {
     
     @Override
     public void execute(Player player, String[] args) {
-        boolean local = hasArg(0, args) && getArg(0, args).equals(localname);
+        boolean local = hasTown(player.getLocation());
         MapView view = newView(getMap(), player, local);
         listener.add(player.getUniqueId(), view);
         view.open();
     }
     
+    private boolean hasTown(Location location) {
+        return getMap().hasTown(Chunk.fromLocation(location));
+    }
+
     /**
      * Returns a new MapView either to either display 
      * the global map or a single town.
@@ -41,10 +41,5 @@ public class MapCmd extends CmdBase {
             return MapFactory.newTownView(map, player, town);
         }
         return MapFactory.newGlobalView(map, player);
-    }
-    
-    @Override
-    public List<String> completeTab(String[] args) {
-        return Util.filterList(Arrays.asList(localname), args[0]);
     }
 }
